dd5cc89af8ca404245e0ae83983850c74ccb2284
CPU-BrandonC <brandon.chesser@c-p-u.com>
2024-02-28 12:45:13 -0600

    Generalized code

--- Remove_STIHL_Fonts.sh
+++ Remove_STIHL_Fonts.sh
@@ -1,6 +1,11 @@
 #!/bin/zsh
 
-currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
+# Font removal script.
 
-sudo rm -f  /Users/$currentUser/Library/Fonts/*STIHL*
-sudo rm -f /Library/Fonts/*STIHL*
+# WARNING!!! THIS SCRIPT REMOVES ALL FONTS WITH A SIMILAR NAME (SEE WILDCARDS)!!!
+
+font_to_remove="FontName"
+current_user=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
+
+sudo rm -f  /Users/$current_user/Library/Fonts/*$font_to_remove*
+sudo rm -f /Library/Fonts/*$font_to_remove*
